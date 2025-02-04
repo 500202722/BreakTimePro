684cc8b6a94429155194f37aab23f0fa74984847
student <500202722@fvtc.edu>
2025-02-04 17:03:16 -0600

    BreakTimePro Init

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BreakTimePro;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
